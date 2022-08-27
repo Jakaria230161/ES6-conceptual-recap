4eb171cbe6aca0b3ed7b4404051e9c7aa0392f2d
Jakaria230161 <jakariamahmud016@gmail.com>
2022-08-27 13:31:32 +0600

    ternary operator

---
+++ js/ternary.js
@@ -0,0 +1,29 @@
+
+const isStudent = true;
+
+if (isStudent === true) {
+    console.log("He is a student");
+}
+
+else {
+    console.log("He is not a student");
+}
+
+// ternary operator
+const sentence = (isStudent === true) ? "he is a student" : "he is not a student";
+console.log(sentence);
+
+
+const isFriend = false;
+
+if (isFriend === true) {
+    console.log("he is my friend");
+}
+
+else {
+    console.log("he is not my friend");
+}
+
+// ternary version
+const makeEasy = (isFriend === true) ? "he is my friend" : "he is not my friend";
+console.log(makeEasy);
