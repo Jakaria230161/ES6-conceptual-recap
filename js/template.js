af65b74ea6dea5373b23b67d7ea83945201da7bc
Jakaria230161 <jakariamahmud016@gmail.com>
2022-08-27 13:21:33 +0600

    template string

---
+++ js/template.js
@@ -0,0 +1,31 @@
+
+const name = "Jakaria";
+const age = 24;
+const job = "Student";
+
+// console.log(name, age, job);
+
+// Dynamically create a new line
+const sentence = `
+My name is ${name}.
+my age is ${age}.
+my job is ${job}.`;
+
+console.log(sentence);
+
+// template string
+const myBio = `My name is Jakaria.
+I am 24 years old.
+I am student at Islamic University.
+I love to learn new things`;
+
+// console.log(myBio);
+
+
+const letConstVar = `
+    1,Breakup with var, we don't use var anymore.Because var can be used for reassign and redefined.
+    2,Let can be reassign but can not be redefined.
+    3,Const can not reassign or redefined.
+        -that's why we understand that if we need to reassign then use let and we didn't any reassign then we use const.
+`
+// console.log(letConstVar);
