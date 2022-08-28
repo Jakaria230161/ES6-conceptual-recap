a03637b10c3bd04b6fe2c44aa5ace66f97f199b5
Jakaria230161 <jakariamahmud016@gmail.com>
2022-08-28 20:45:37 +0600

    arrow functions

---
+++ js/arrow.js
@@ -0,0 +1,10 @@
+// arrow functions
+const square = x => x * 2;
+
+const result = square(4);
+console.log(result);
+
+const num = [3, 4, 5, 7];
+const multiple = num.map(x => x * 2);
+
+console.log(multiple);
